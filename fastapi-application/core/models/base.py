eadb1b0789d2507015e91f4ced18756d4a311b94
Komilov Shakhzod <proap7875@gmail.com>
2025-06-17 20:13:21 +0500

    create int id primary key

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,10 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import (
-    DeclarativeBase,
-    Mapped,
-    mapped_column,
-    declared_attr,
-)
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 from core.config import settings
 from utils import camel_case_to_snake_case
@@ -14,11 +9,9 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     metadata = MetaData(
-        naming_convention=settings.db.naming_conventions
+        naming_convention=settings.db.naming_conventions,
     )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
---
+++ fastapi-application/core/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from .mixins.pk import PKMixin
+
+class User(PKMixin, Base):
+    username: Mapped[str] = mapped_column(unique=True)
+    foo: Mapped[int]
+    bar: Mapped[int]
+
+    __table_args__ = (
+        UniqueConstraint(
+            "foo",
+            "bar",
+        ),
+    )
