87bfe1cb71e6fa5e8b663507f31673d6efd5f677
Komilov Shakhzod <proap7875@gmail.com>
2025-06-18 13:14:25 +0500

    create user model with fastapi-users

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,9 +1,12 @@
+from fastapi_users.db import SQLAlchemyBaseUserTable
+
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 from .mixins.pk import PKMixin
 
+# Base example
 class User(PKMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
     foo: Mapped[int]
@@ -15,3 +18,9 @@ class User(PKMixin, Base):
             "bar",
         ),
     )
+
+
+# Example with fastapi-users
+# class User(Base, PKMixin, SQLAlchemyBaseUserTable[int]):
+#     pass
+
