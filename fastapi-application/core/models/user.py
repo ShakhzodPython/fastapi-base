3c20e78f249fc93b531212b32746233a380cce23
Komilov Shakhzod <proap7875@gmail.com>
2025-06-18 13:27:08 +0500

    create migration for user table with fastapi-user

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,26 +1,26 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable
 
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+# from sqlalchemy import UniqueConstraint
+# from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 from .mixins.pk import PKMixin
 
-# Base example
-class User(PKMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
-    foo: Mapped[int]
-    bar: Mapped[int]
 
-    __table_args__ = (
-        UniqueConstraint(
-            "foo",
-            "bar",
-        ),
-    )
+# Base example
+# class User(PKMixin, Base):
+#     username: Mapped[str] = mapped_column(unique=True)
+#     foo: Mapped[int]
+#     bar: Mapped[int]
+#
+#     __table_args__ = (
+#         UniqueConstraint(
+#             "foo",
+#             "bar",
+#         ),
+#     )
 
 
 # Example with fastapi-users
-# class User(Base, PKMixin, SQLAlchemyBaseUserTable[int]):
-#     pass
-
+class User(Base, PKMixin, SQLAlchemyBaseUserTable[int]):
+    pass
